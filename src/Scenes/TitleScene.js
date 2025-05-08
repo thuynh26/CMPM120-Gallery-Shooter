82e45b5d97581e21a4cf506175b0490f93fd2d23
tnhuynh05 <tnhuynh05@gmail.com>
2025-05-08 02:11:16 -0700

    updated title scene

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -3,19 +3,38 @@ class TitleScene extends Phaser.Scene {
         super("titleScene");
     }
 
+    preload() {
+        this.load.setPath("./assets/");
+        this.load.image("titleBackground", "background.png");
+    }
+
     create() {
-        this.add.text(480, 200, "🐼 Zookeeper 🐍", {
+        this.add.image(480, 240, "titleBackground").setDisplaySize(960, 480);
+
+        this.add.text(480, 200, "🐼 Zookeeper 🐣", {
             fontFamily: "Titan One",
             fontSize: "48px",
-            color: "#ffffff"
+            color: "#ffffff",
+            stroke: "#2E6F40",
+            strokeThickness: 8,
             
         }).setOrigin(0.5);
 
-        this.add.text(480, 280, "Press SPACE to Start", {
+        let startText = this.add.text(480, 280, "Press SPACE to Start!", {
             fontFamily: "Titan One",
             fontSize: "24px",
-            color: "#eeeeee"
+            color: "#eeeeee",
+            stroke: "#2E6F40",
+            strokeThickness: 6,
         }).setOrigin(0.5);
+
+        this.tweens.add({
+            targets: startText,
+            alpha: { from: 1, to: 0.4 },
+            duration: 800,
+            yoyo: true,
+            repeat: -1
+          });
 
         this.input.keyboard.once('keydown-SPACE', () => {
             this.scene.start("gameScene");
